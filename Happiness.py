d9820d426bcb480b372a2eca7e7a9f83b46ec622
Doctor Magorium <ewanrcoding@gmail.com>
2025-11-18 17:25:46 +0000

    hello

---
+++ Happiness.py
@@ -0,0 +1,4 @@
+variablenotion = "exhiauhiouhouwheuofho08u092834yt890y83hy8ryhf0"
+y = input("System Powered?")
+if y == "yes":
+    print(f"Initiating faster printing;;; API key = {variablenotion}")
